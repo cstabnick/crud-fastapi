8355f1f4e6b8522ed87c7f69b2b038d9ae34825e
Connor Stabnick <cstabnick@gmail.com>
2022-01-14 00:52:37 -0500

    more users cleanup

--- api/users.py
+++ api/users.py
@@ -1,11 +1,6 @@
-import time
 from fastapi import APIRouter, HTTPException, Request
-from typing import Optional, List
-
-from pydantic import BaseModel
-import psycopg2
+from typing import List
 import bcrypt
-from datetime import datetime
 
 from lib.users import ITUsers
 from lib.util import ITUtil
@@ -13,31 +8,9 @@ from lib.util import ITUtil
 router = APIRouter()
 
 class ITUsersAPI: 
-    class UsersModel(BaseModel):
-        user_id: Optional[int]
-        username: Optional[str]
-        password: Optional[str]
-        email: Optional[str]
-        created_at: Optional[datetime]
-        updated_at: Optional[datetime]
-        
-        current_session_id: Optional[int]
-
-        @staticmethod
-        def fields_not_in_db():
-            return ["current_session_id"]
-
-        @staticmethod        
-        def required_on_create_fields():
-            return ["username", "password", "email"]
-        
-        @staticmethod
-        def fields_not_returned():
-            return ["password"]
-    
     @staticmethod
     @router.post("/users/login")
-    def login_user(user: UsersModel, response_model=UsersModel):
+    def login_user(user: ITUsers.UsersModel, response_model=ITUsers.UsersModel):
         login_sql = """
             select user_id, password 
             from users 
@@ -53,15 +26,15 @@ class ITUsersAPI:
             raise HTTPException(status_code=401, detail="Bad!")
 
         if bcrypt.checkpw(user.password.encode(), hashed):
-            user = ITUtil.get_by_model(ITUsersAPI.UsersModel(), 1, 0, True, {"user_id": qry["user_id"]})[0]
+            user = ITUtil.get_by_model(ITUsers.UsersModel(), 1, 0, True, {"user_id": qry["user_id"]})[0]
             user['current_session_id'] = ITUsers.update_session(user['user_id'])['session_id']
             return user
         else:
             raise HTTPException(status_code=401, detail="Bad!")
 
     @staticmethod
-    @router.post("/users/create", response_model=UsersModel)
-    def create_user(user: UsersModel, request: Request):
+    @router.post("/users/create", response_model=ITUsers.UsersModel)
+    def create_user(user: ITUsers.UsersModel, request: Request):
         password = user.password.encode()
 
         salt = bcrypt.gensalt()
@@ -71,7 +44,6 @@ class ITUsersAPI:
         user['current_session_id'] = ITUsers.update_session(user['user_id'])['session_id']
 
         return user
-        
 
     @staticmethod
     @router.delete("/users/{user_id}")
@@ -82,13 +54,14 @@ class ITUsersAPI:
         return ITUtil.pg_exec_no_return(sql, [user_id])
 
     @staticmethod
-    @router.get("/users", response_model=List[UsersModel])
+    @router.get("/users", response_model=List[ITUsers.UsersModel])
     def read_users(limit: int = 100, skip: int = 0):
-        users = ITUtil.get_by_model(ITUsersAPI.UsersModel(), limit, skip)
+        users = ITUtil.get_by_model(ITUsers.UsersModel(), limit, skip)
         return users
 
     @staticmethod
-    @router.get("/users/{user_id}", response_model=UsersModel)
+    @router.get("/users/{user_id}", response_model=ITUsers.UsersModel)
     def read_user(user_id: int):
-        return ITUtil.get_by_model(ITUsersAPI.UsersModel(), 1, 0, True, {"user_id": user_id})
+        user = ITUtil.get_by_model(ITUsers.UsersModel(), 1, 0, True, {"user_id": user_id})[0]
+        return user
         
--- lib/users.py
+++ lib/users.py
@@ -1,6 +1,31 @@
 from lib.util import ITUtil
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
 
 class ITUsers:
+    class UsersModel(BaseModel):
+        user_id: Optional[int]
+        username: Optional[str]
+        password: Optional[str]
+        email: Optional[str]
+        created_at: Optional[datetime]
+        updated_at: Optional[datetime]
+        
+        current_session_id: Optional[int]
+
+        @staticmethod
+        def fields_not_in_db():
+            return ["current_session_id"]
+
+        @staticmethod        
+        def required_on_create_fields():
+            return ["username", "password", "email"]
+        
+        @staticmethod
+        def fields_not_returned():
+            return ["password"]
+            
     @staticmethod
     def update_session(user_id: int):
         return ITUtil.pg_select_one("select update_session(%(user_id)s) as session_id", {"user_id": user_id}, True)
