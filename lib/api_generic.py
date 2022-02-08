cedef64e6b17620d805838058034e84a8f89edf1
Connor Stabnick <cstabnick@gmail.com>
2022-02-08 01:19:38 -0500

    Resolves TypeError: 'staticmethod' object is not callable

--- lib/api_generic.py
+++ lib/api_generic.py
@@ -25,7 +25,6 @@ class APIGeneric:
         if "delete" in endpoints_to_create:
             python += f"""
    
-@staticmethod
 def delete_{db_table_name_singular}({db_table_id}: int):
     sql = \"\"\"
         update {db_table_name} set is_deleted = true where {db_table_id} = %s
@@ -33,7 +32,6 @@ def delete_{db_table_name_singular}({db_table_id}: int):
     return ITUtil.pg_exec_no_return(sql, [{db_table_id}])
 setattr(it_class, 'delete_{db_table_name_singular}', staticmethod(delete_{db_table_name_singular}))
 
-@staticmethod
 @router.delete("/{db_table_name}/{{{db_table_id}}}")
 def delete_{db_table_name_singular}_api({db_table_id}: int):
     return {it_class_name}.delete_{db_table_name_singular}({db_table_id})
@@ -44,24 +42,20 @@ setattr(it_api_class, 'delete_{db_table_name_singular}_api', staticmethod(delete
         if "read" in endpoints_to_create:
             python += f"""
 
-@staticmethod
 def read_{db_table_name}(limit: int = 100, skip: int = 0):
     return ITUtil.get_by_model({model_name}(), limit, skip)
 setattr(it_class, 'read_{db_table_name}', staticmethod(read_{db_table_name}))
 
-@staticmethod
 @router.get("/{db_table_name}", response_model=List[{model_name}])
 def read_{db_table_name}_api(limit: int = 100, skip: int = 0):
     return {it_class_name}.read_{db_table_name}(limit, skip)
 setattr(it_api_class, 'read_{db_table_name}_api', staticmethod(read_{db_table_name}_api))
 
 
-@staticmethod
 def read_{db_table_name_singular}({db_table_id}: int):
     return ITUtil.get_by_model_id({model_name}(), {{"{db_table_id}": {db_table_id}}})
 setattr(it_class, 'read_{db_table_name_singular}', staticmethod(read_{db_table_name_singular}))
 
-@staticmethod
 @router.get("/{db_table_name}/{{{db_table_id}}}", response_model={model_name})
 def read_{db_table_name_singular}_api({db_table_id}: int):
     return {it_class_name}.read_{db_table_name_singular}({db_table_id})
